13e401b9fd3bfb87d6cfc5e0c4a86e3c84492585
James Schofield Hayes <james.s.hayes@gmail.com>
2021-01-04 19:33:14 -0700

    empty LoggingManager class

---
+++ TBXTools/LoggingManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TBXTools
+{
+    static class LoggingManager
+    {
+        // TODO - create logging manager
+    }
+}
