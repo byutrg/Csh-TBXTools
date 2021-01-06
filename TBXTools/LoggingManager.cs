8d6954c2bce6b0981f10a47eb6bf06cabed9d553
James Hayes <james.s.hayes@gmail.com>
2021-01-05 19:54:45 -0700

    handle LoggingLevel checks internally
    (rather than forcing users to write delegates that check LoggingLevel)

--- TBXTools/LoggingManager.cs
+++ TBXTools/LoggingManager.cs
@@ -7,6 +7,8 @@ namespace TBXTools
 {
     public static class LoggingManager
     {
+        public delegate void OutputFunc(string message, params object[] args);
+
         public enum LoggingLevels
         {
             None,
@@ -15,13 +17,52 @@ namespace TBXTools
             Verbose
         }
 
-        public static LoggingLevels LoggingLevel { get; set; } = LoggingLevels.Info;
+        public static LoggingLevels LoggingLevel { get; private set; } = LoggingLevels.Info;
 
-        public delegate void OutputFunc(string message, params object[] args);
-        public static OutputFunc Output { get; set; } = (message, args) => { if (LoggingLevel > LoggingLevels.None) Console.WriteLine(message); };
+        private static OutputFunc output = (message, args) => { if (LoggingLevel > LoggingLevels.None) Console.WriteLine(message); };
+        private static OutputFunc outputError = (message, args) => { if (LoggingLevel >= LoggingLevels.ErrorsOnly) Output(message, args); };
+        private static OutputFunc outputInfo = (message, args) => { if (LoggingLevel >= LoggingLevels.Info) Output(message, args); };
+        private static OutputFunc outputVerbose = (message, args) => { if (LoggingLevel >= LoggingLevels.Verbose) Output(message, args); };
 
-        public static OutputFunc OutputError { get; set; } = (message, args) => { if (LoggingLevel >= LoggingLevels.ErrorsOnly) Output(message, args); };
-        public static OutputFunc OutputInfo { get; set; } = (message, args) => { if (LoggingLevel >= LoggingLevels.Info) Output(message, args); };
-        public static OutputFunc OutputVerbose { get; set; } = (message, args) => { if (LoggingLevel >= LoggingLevels.Verbose) Output(message, args); };
+        private static OutputFunc SetInternalOutputFunc(OutputFunc func, LoggingLevels level) => (message, args) => { if (LoggingLevel >= level) func(message, args); };
+
+        public static OutputFunc Output {
+            get => output;
+            private set => output = SetInternalOutputFunc(value, LoggingLevels.None);
+        } 
+        public static OutputFunc OutputError {
+            get => outputError;
+            private set => outputError = SetInternalOutputFunc(value, LoggingLevels.ErrorsOnly);
+        } 
+        public static OutputFunc OutputInfo {
+            get => outputInfo;
+            private set => outputInfo = SetInternalOutputFunc(value, LoggingLevels.Info);
+        }
+        public static OutputFunc OutputVerbose {
+            get => outputVerbose;
+            private set => outputVerbose = SetInternalOutputFunc(value, LoggingLevels.Verbose);
+        } 
+
+        public static void SetLoggingLevel(LoggingLevels level) => LoggingLevel = level;
+        /// <summary>
+        /// Set Output Function. Will be invoked only if LoggingLevel >= LoggingLevels.None.
+        /// </summary>
+        /// <param name="function"></param>
+        public static void SetOutputFunction(OutputFunc function) => Output = function;
+        /// <summary>
+        /// Set OutputError Function. Will be invoked only if LoggingLevel >= LoggingLevels.ErrorsOnly.
+        /// </summary>
+        /// <param name="function"></param>
+        public static void SetOutputErrorFunction(OutputFunc function) => OutputError = function;
+        /// <summary>
+        /// Set OutputInfo Function. Will be invoked only if LoggingLevel >= LoggingLevels.Info.
+        /// </summary>
+        /// <param name="function"></param>
+        public static void SetOutputInfoFunction(OutputFunc function) => OutputInfo = function;
+        /// <summary>
+        /// Set OutputVerbose Function. Will be invoked only if LoggingLevel >= LoggingLevels.Verbose.
+        /// </summary>
+        /// <param name="function"></param>
+        public static void SetOutputVerboseFunction(OutputFunc function) => OutputVerbose = function;
     }
 }
